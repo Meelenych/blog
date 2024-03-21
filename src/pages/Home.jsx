342cd9fbaf5f645979046e619890752e027e6530
Meelenych <meelenych@gmail.com>
2024-03-21 00:04:01 -0600

    home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div className='m-10'>
+			<h1 className='sm:text4xl text-2xl font-bold mb-7 text-sky-800'>
+				Hello! Welcome to my blog!
+			</h1>
+			<p className='mx-auto leading-relaxed text-base mb-4'>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt non sed
+				officiis dicta facilis at vel libero quo. Tenetur eaque dolor, ducimus vel,
+				cum incidunt dolores iure quas numquam necessitatibus non sunt distinctio
+				laudantium laborum nam minus aut doloremque vero?
+			</p>
+			<p className='mx-auto leading-relaxed text-base mb-4'>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. A vel magnam
+				natus, omnis veniam quos molestiae eius at deserunt ipsam. Maiores vel
+				officiis fuga repudiandae eos et delectus ut sit!
+			</p>
+			<p className='mx-auto leading-relaxed text-base mb-4'>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque accusantium
+				voluptatum non corporis ipsa temporibus odio, cum maiores laborum fugit esse
+				atque perspiciatis ullam deserunt mollitia. Explicabo rerum porro cumque
+				odio maiores corporis voluptate, molestiae ipsa beatae impedit eligendi
+				eaque fugiat obcaecati! Nostrum, quasi eligendi. Repellendus quasi unde
+				nulla, at odit sint ipsam excepturi accusantium cumque incidunt est
+				cupiditate. Hic?
+			</p>
+		</div>
+	);
+};
+
+export default Home;
